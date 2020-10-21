8709e1b4eaca204cc9952cf0c41444009b4be475
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2020-10-21 15:48:03 +0200

    moulinette 1 moitié

---
+++ intro/exercise-1/string.js
@@ -0,0 +1,28 @@
+function ucfirst(string){
+    if(typeof(string) !== 'string' || string.trim().length ==0)
+        return "";
+
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+console.log(ucfirst("BonNjouer"))
+
+function capitalize(string){
+    if(typeof(string) !== 'string' || string.trim().length ==0)
+    return "";
+    return string.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ');
+}
+
+function camelCase(string) {
+    if (typeof string !== "string" || string === "") return "";
+    return string.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join('');
+}
+
+function snake_case(string) {
+    if (typeof string !== "string" || string === "") return "";
+    return string.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join('_').toLowerCase();
+}
+function leet(string) {
+    if (typeof string !== "string" || string === "") return "";
+    var monCryptage = { "A": 4, "E": 3, "I": 1, "O": "0", "U": "(_)", "Y": 7 };
+    return string.split('').map(s => monCryptage[s.toUpperCase()] || s).join('')
+}
