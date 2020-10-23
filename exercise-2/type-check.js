48662cc2dd8ef607a233b3cd2e5496a81ba7e007
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2020-10-23 19:01:10 +0200

    commit exercice 2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -4,17 +4,19 @@ function type_check_v1(myNumber, typecheck){
         case 'StringNotNumber':
         case 'BooleanNotNumber':
         case 'object':
+        case 'function':
         case 'string':
             return (typeof(myNumber) === typecheck);
           break;
         case 'undefined':
             return myNumber === undefined;
             case 'null':
+            case 'nullNotObject':
                 return myNumber === null;
+            case 'array':
+                return Array.isArray(myNumber);
         default:
          return false;
       }
 }
-str = null;
-console.log(type_check_v1(str,'null'))
 
