1b430fb6a80197af68acc7508f3313caca8efb74
Yassine Bousaidi <Yassine.bousaidi@hotmail.com>
2020-10-23 18:26:15 +0200

    commit exercice 2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,13 +1,16 @@
-function type_check_v1(myNumber, typecheck ='number'){
-    if(typeof(myNumber) == "object"){
-        if(Array.isArray(myNumber) && typecheck == "array"){
-            return true;
-        }
-    }
-    else {
-        return false;
-    }
+function type_check_v1(myNumber, typecheck){
+    switch (typecheck) {
+        case 'number':
+        case 'StringNotNumber':
+        case 'BooleanNotNumber':
+        case 'object':
+            return (typeof(myNumber)== typecheck);
 
-        return (typeof(myNumber) == typecheck);
+          console.log('Mangoes and papayas are $2.79 a pound.');
+          // expected output: "Mangoes and papayas are $2.79 a pound."
+          break;
+        default:
+         return false;
+      }
 }
 
